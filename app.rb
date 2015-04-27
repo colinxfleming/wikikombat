7a385b01bd564890e70ff07fb813bbbd2effe714
colinxfleming <c3flemin@gmail.com>
2015-04-26 23:23:00 -0400

    thanks gitignore; also comment up app and make sample config.yml

--- app.rb
+++ app.rb
@@ -6,6 +6,7 @@ require 'erubis'
 require 'json'
 require 'httparty'
 
+# note that this gem requires tilt 1.4.1 and breaks on tilt 2.0.1. If it screws up try gem uninstall tilt -v 2.0.1
 require 'sinatra/config_file' # uncomment these lines and fill out config.yml if you need secrets
 config_file './config/config.yml'
 
@@ -14,7 +15,7 @@ config_file './config/config.yml'
 	Dir.entries("./#{dir}").select { |f| !File.directory? f }.each { |file| require_relative "./#{dir}/#{file}" }
 end
 
-set :title, "Sinatra-Skeleton"
+set :title, settings.title
 
 # db stuff
 configure :development do 
@@ -47,17 +48,17 @@ end
 
 # create route
 post '/', provides: :json do 
+  #  room for improvement: checking against the db first
+  # lowercasing everything
+  #  enforcing uniqueness db side
+
   if params['text_input'].strip != ''
     # take input and get wikipedia page for that thing
     # not fancy enough to handle multiple entries for something with the same name
     input = URI.escape params['text_input']
     wikipedia_url = "http://en.wikipedia.org/w/api.php?format=json&action=query&titles=#{input}&prop=revisions&rvprop=content"
 
-    user_agent = "WikiKombat (http://skeletonsinatra.herokuapp.com/; MyCoolTool@example.com) Jokey Sinatra app that compares the lengths of two Wikipedia pages"
-
-    headers = {"User-Agent" => user_agent}
-
-    response = HTTParty.get(wikipedia_url, headers: headers).parsed_response.to_json
+    response = HTTParty.get(wikipedia_url, headers: {"User-Agent" => settings.user_agent}).parsed_response.to_json
 
     mk = Request.where(name: 'Mortal Kombat').first
 
