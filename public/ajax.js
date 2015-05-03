ce2caa9cdb50d891e9b723a82c63a977e7db4d24
Colin Fleming <c3flemin@gmail.com>
2015-05-03 15:42:00 -0400

    js cleanup

--- public/ajax.js
+++ public/ajax.js
@@ -9,18 +9,14 @@ $(document).ready(function() {
 				$('#result').html(data.msg);
 				$('#result_table').empty().append('<tr><th>Wikipedia Page</th><th>More Important Than Mortal Kombat?</th></tr>');
 
-
-for (i = 0; i < data.roll.length; i++) { 
-	if (data.roll[i].longer_than_mk === true) { 
-		longer_than_mk = 'WAY more important!'
-	} else {
-		longer_than_mk = 'no :-('
-	}
-	$('#result_table').append('<tr class="result_row"><td>' + data.roll[i].name + '</td><td>' + longer_than_mk + '</td></tr>');
-};
-				// $.each(data.roll, function(d) { 
-					// $('#result_table').append('<tr class="result_row"><td>' + d.name + '</td><td>' + d.longer_than_mk + '</td></tr>');
-				// });
+				for (i = 0; i < data.roll.length; i++) { 
+					if (data.roll[i].longer_than_mk === true) { 
+						longer_than_mk = 'WAY more important!'
+					} else {
+						longer_than_mk = 'no :-('
+					}
+					$('#result_table').append('<tr class="result_row"><td>' + data.roll[i].name + '</td><td>' + longer_than_mk + '</td></tr>');
+				};
 			},
 			"json"
 		);
